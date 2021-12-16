cb40303254906a968e1776aec271a7e63eca34b3
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-16 16:28:05 +0500

    Create task5.cpp

---
+++ arrays.md/task5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int main() {
+    int n;
+    std::cout << "enter n: ";
+    std::cin >> n;
+    int* array = new int[n];
+    std::cout << "enter elements of array: ";
+    for (int i = 0; i < n; ++i)
+        std::cin >> array[i];
+    int max_sum = INT_MIN;
+    int first_max, second_max;
+    for (int i = 0; i < n - 1; ++i) {
+        if (array[i] + array[i + 1] > max_sum) {
+            first_max = array[i];
+            second_max = array[i + 1];
+            max_sum = array[i] + array[i + 1];
+        }
+    }
+    std::cout << "first and second maximums: " << first_max << ' ' << second_max << '\n';
+    delete[] array;
+}
