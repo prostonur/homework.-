79a9b1d537a0399ee211d4c3fe836e7d5c9740fd
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-16 23:26:35 +0500

    Create task3.cpp

---
+++ loops.md/task3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int n;
+    double x;
+    std::cout << "enter n, x: ";
+    std::cin >> n >> x;
+    double s = 0. ;
+    double t = sin(x);
+    for (int i = 1; i <= n; ++i) {
+        s += t;
+        t = sin(t);
+    }
+    std::cout << sin(s) << '\n';
+}
