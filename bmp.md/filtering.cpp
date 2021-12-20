88b9d6fec9189550016a6c21cc62e43fab2699e9
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-20 12:28:36 +0500

    Update filtering.cpp

--- bmp.md/filtering.cpp
+++ bmp.md/filtering.cpp
@@ -1,92 +1,70 @@
+#include "BmpStructures.h"
 #include "ImgFunctions.h"
-#include <cmath>
+#include <algorithm> 
 
-RgbImg convolution(RgbImg const& img, size_t kernel_size)
+typedef unsigned long ulong;
+
+RgbImg medianFilter(RgbImg const& image, ulong kernel_size)
 {
-    size_t const kernel_count = kernel_size * kernel_size;
-    double const kernel_el = 1. / kernel_count;
-    double** kernel = new double* [kernel_size];
-    for (size_t row = 0; row < kernel_size; ++row)
-    {
-        kernel[row] = new double[kernel_size];
-        for (size_t col = 0; col < kernel_size; ++col)
-            kernel[row][col] = kernel_el;
-    }
-    RgbImg result = convolution(img, kernel_size, kernel);
-    for (size_t row = 0; row < kernel_size; ++row)
-        delete[] kernel[row];
-    delete[] kernel;
-    return result;
-}
+    RgbImg imgCopy = copyRgbImg(image);
+    ulong const height = image.height;
+    ulong const width = image.width;
+    ulong const half = kernel_size / 2;
+    ulong const kernel_count = kernel_size * kernel_size;
 
-RgbImg convolution(RgbImg const& img, size_t kernel_size, double** kernel)
-{
-    RgbImg imgCopy = copyRgbImg(img);
-    size_t const height = img.height;
-    size_t const width = img.width;
-    size_t const half = kernel_size / 2;
+    double* blues = new double[kernel_count];
+    double* greens = new double[kernel_count];
+    double* reds = new double[kernel_count];
 
-    for (size_t row = half; row < height - half; ++row)
-        for (size_t col = half; col < width - half; ++col)
+    auto median_element_index = kernel_count / 2;
+
+    for (ulong row = half; row < height - half; ++row)
+        for (ulong col = half; col < width - half; ++col)
         {
-            double b_weighted_sum = 0;
-            double g_weighted_sum = 0;
-            double r_weighted_sum = 0;
-            for (size_t k_row = 0; k_row < kernel_size; ++k_row)
-                for (size_t k_col = 0; k_col < kernel_size; ++k_col)
+            for (ulong k_row = 0; k_row < kernel_size; ++k_row)
+                for (ulong k_col = 0; k_col < kernel_size; ++k_col)
                 {
-                    size_t const res_row = row + k_row - half;
-                    size_t const res_col = col + k_col - half;
-                    RGB const& pixel = img.pixels[res_row][res_col];
-                    double kern_el = kernel[k_row][k_col];
-                    b_weighted_sum += kern_el*pixel.Blue;
-                    g_weighted_sum += kern_el*pixel.Green;
-                    r_weighted_sum += kern_el*pixel.Red;
+                    ulong const res_row = row + k_row - half;
+                    ulong const res_col = col + k_col - half;
+                    RGB const& pixel = image.pixels[res_row][res_col];
+                    auto idx = (k_row * kernel_size) + k_col;
+
+                    blues[idx] = pixel.Blue;
+                    greens[idx] = pixel.Green;
+                    reds[idx] = pixel.Red;
                 }
+
             RGB& pixel = imgCopy.pixels[row][col];
-                double blue = std::abs(b_weighted_sum);
-                double green = std::abs(g_weighted_sum);
-                double red = std::abs(r_weighted_sum);
-                pixel.Blue = blue > 255 ? 255 : blue;
-                pixel.Green = green > 255 ? 255 : green;
-                pixel.Red = red > 255 ? 255 : red;
+
+            std::sort(blues, blues + (int)kernel_count);
+            std::sort(greens, greens + (int)kernel_count);
+            std::sort(reds, reds + (int)kernel_count);
+
+            auto median_blue = blues[median_element_index];
+            auto median_green = greens[median_element_index];
+            auto median_red = reds[median_element_index];
+
+            pixel.Blue = median_blue;
+            pixel.Green = median_green;
+            pixel.Red = median_red;
         }
+
+    delete[] blues;
+    delete[] greens;
+    delete[] reds;
+
     return imgCopy;
 }
 
-RgbImg contours(RgbImg const& img)
+int main(int argc, char const* argv[])
 {
-    size_t const kern_sz = 3;
-    static double kern_hor[kern_sz][kern_sz] = {
-            {-1, 0, 1},
-            {-2, 0, 2},
-            {-1, 0, 1}
-    };
-    static double kern_ver[kern_sz][kern_sz] = {
-            {-1, -2, -1},
-            { 0,  0,  0},
-            { 1,  2,  1}
-    };
-    static double* ver_ptr[kern_sz] = { kern_hor[0], kern_hor[1], kern_hor[2] };
-    static double* hor_ptr[kern_sz] = { kern_ver[0], kern_ver[1], kern_ver[2] };
-    RgbImg imgCopyHor = convolution(img, kern_sz, hor_ptr);
-    RgbImg imgCopyVer = convolution(img, kern_sz, ver_ptr);
-    RgbImg result = copyRgbImg(img);
-    for (size_t row = 0; row < img.height; ++row)
-        for (size_t col = 0; col < img.width; ++col)
-        {
-            RGB& c_h = imgCopyHor.pixels[row][col];
-            RGB& c_v = imgCopyVer.pixels[row][col];
-            RGB& c_r = result.pixels[row][col];
+    auto image = readRgbImg("kidsnoise.bmp");
+    auto medianed = medianFilter(image, 3);
 
-            double blue = sqrt((double)c_h.Blue * c_h.Blue + (double)c_v.Blue * c_v.Blue);
-            double green = sqrt((double)c_h.Green * c_h.Green + (double)c_v.Green * c_v.Green);
-            double red = sqrt((double)c_h.Red * c_h.Red + (double)c_v.Red * c_v.Red);
+    writeRgbImg("kidsnoise_1.bmp", medianed);
 
-            c_r.Blue = blue > 255 ? 255 : blue;
-            c_r.Green = green > 255 ? 255 : green;
-            c_r.Red = red > 255 ? 255 : red;
-        }
-    autolevels(result, kern_sz);
-        return result;
+    deleteRgbImg(image);
+    deleteRgbImg(medianed);
+
+    return 0;
 }
