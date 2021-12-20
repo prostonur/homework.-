8fc0f0c242ae508dbdd70267b106e18aeb964285
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-20 12:11:43 +0500

    Create BasicMathematicOperations.cpp

---
+++ codewars/BasicMathematicOperations.cpp
@@ -0,0 +1,18 @@
+int basicOp(char op, int val1, int val2) {
+  if (op == '+')
+      {
+        return val1+val2;
+      }
+  if (op == '-')
+      {
+        return val1-val2;
+      }
+  if (op == '*')
+      {
+        return val1*val2;
+      }
+  if (op == '/')
+      {
+        return val1/val2;
+      }
+}
