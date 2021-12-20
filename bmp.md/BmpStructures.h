64734d6b6223d8cdd13dcafa9d8837918edee441
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-20 12:23:39 +0500

    Update BmpStructures.h

--- bmp.md/BmpStructures.h
+++ bmp.md/BmpStructures.h
@@ -1,57 +1,60 @@
-#ifndef BMPSTRUCTURES_H
-#define BMPSTRUCTURES_H
-#include <cstddef>
-typedef unsigned short WORD;
-typedef unsigned int DWORD;
-typedef int LONG;
-typedef unsigned char BYTE;
+#include <iostream>
+#include "BmpStructures.h"
+#include "ImgFunctions.h"
 
-//Следующая строчка отменяет выравнивание
-//типов данных для структур
-#pragma pack(push,1)
-struct BITMAPFILEHEADER {
-    WORD bfType;
-    DWORD bfSize;
-    WORD bfReserved1;
-    WORD bfReserved2;
-    DWORD bfOffBits;
-};
-
-struct BITMAPINFOHEADER {
-    DWORD biSize;
-    LONG biWidth;
-    LONG biHeight;
-    WORD biPlanes;
-    WORD biBitCount;
-    DWORD biCompression;
-    DWORD biSizeImage;
-    LONG biXPelsPerMeter;
-    LONG biYPelsPerMeter;
-    DWORD biClrUsed;
-    DWORD biClrImportant;
-};
-
-struct RGBQUAD {
-    BYTE rgbBlue;
-    BYTE rgbGreen;
-    BYTE rgbRed;
-    BYTE rgbReserved;
-};
-
-struct RGB
-        {
-    BYTE Blue;
-    BYTE Green;
-    BYTE Red;
-        };
-
-struct RgbImg {
-    RGB** pixels;
-    size_t height;
-    size_t width;
-};
-
-//А эта строчка отменяет отмену выравнивания :)
-#pragma pack(pop)
-
-#endif
+int main()
+{
+RgbImg img = createRgbImg(100, 700);
+for (size_t row = 0; row < 100;++row)
+	for (size_t col = 0; col < 100; ++col)
+	{
+		img.pixels[row][col].Blue = 0;
+		img.pixels[row][col].Green = 0;
+		img.pixels[row][col].Red = 255;
+	}
+for (size_t row = 0; row < 100;++row)
+	for (size_t col = 100; col < 200; ++col)
+	{
+		img.pixels[row][col].Blue = 0;
+		img.pixels[row][col].Green = 127;
+		img.pixels[row][col].Red = 255;
+	}
+for (size_t row = 0; row < 100;++row)
+	for (size_t col = 200; col < 300; ++col)
+	{
+		img.pixels[row][col].Blue = 0;
+		img.pixels[row][col].Green = 255;
+		img.pixels[row][col].Red = 255;
+	}
+for (size_t row = 0; row < 100;++row)
+	for (size_t col = 300; col < 400; ++col)
+	{
+		img.pixels[row][col].Blue = 0;
+		img.pixels[row][col].Green = 255;
+		img.pixels[row][col].Red = 0;
+	}
+for (size_t row = 0; row < 100;++row)
+	for (size_t col = 400; col < 500; ++col)
+	{
+		img.pixels[row][col].Blue = 255;
+		img.pixels[row][col].Green = 191;
+		img.pixels[row][col].Red = 0;
+	}
+for (size_t row = 0; row < 100;++row)
+	for (size_t col = 500; col < 600; ++col)
+	{
+		img.pixels[row][col].Blue = 255;
+		img.pixels[row][col].Green = 0;
+		img.pixels[row][col].Red = 0;
+	}
+for (size_t row = 0; row < 100;++row)
+	for (size_t col = 600; col < 700; ++col)
+	{
+		img.pixels[row][col].Blue = 130;
+		img.pixels[row][col].Green = 0;
+		img.pixels[row][col].Red = 75;
+	}
+writeRgbImg("Rainbow.bmp", img);
+deleteRgbImg(img);
+return 0;
+	}
