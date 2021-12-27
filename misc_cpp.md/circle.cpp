217d8be16826801841cc279a452546fdab16bb43
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-28 04:57:40 +0500

    Update circle.cpp

--- misc_cpp.md/circle.cpp
+++ misc_cpp.md/circle.cpp
@@ -1,27 +1,20 @@
-#include <iostream>
-#include<cmath>
-
-int main()
-{
-    double br, lr, nr, x, y;
-    std::cout << "Enter the radius of the big circle:\n";
-    std::cin >> br;
-    std::cout << "Enter the ragius of the little circle:\n";
-    std::cin >> lr;
-    std::cout << "Enter x:\n";
-    std::cin >> x;
-    std::cout << "Enter y:\n";
-    std::cin >> y;
-
-    nr = sqrt(x * x) + (y * y);
-    if (lr < nr < br)
-    {
-        std::cout << "In the circle" << std::endl;
-    }
-    else
-    {
-        std::cout << "Out of the circle" << std::endl;
-    }
-    return 0;
-
-}
+int x, y;
+std::cout << "enter x, y: ";
+std::cin >> x >> y;
+int r, R;
+std::cout << "enter r, R: ";
+std::cin >> r >> R;
+if (sqrt(pow(x, 2) + pow(y, 2)) <= pow(R, 2) and sqrt(pow(x, 2) + pow(y, 2)) >= pow(r, 2))
+std::cout << "Mur <3\n";
+else
+std::cout << "bob\n";
+double x, y;
+std::cout <<"Enter x, y: ";
+std::cin >> x >> y;
+double r, R;
+std::cout << "enter r, R: ";
+std::cin >> r >> R;
+if (sqrt(pow(x, 2) + pow(y, 2)) <= pow(R, 2) and sqrt(pow(x, 2) + pow(y, 2))>= pow(r, 2))
+std::cout << "Mur <3\n";
+else
+std::cout << "bob\n";
