ebb95c5e7878d0bcd19bb0b36fb93bd409fd75ac
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-28 01:29:37 +0300

    Update task2.cpp

--- files.md/task2.cpp
+++ files.md/task2.cpp
@@ -1,81 +1,76 @@
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-
-int file2hex(int argc, const char* argv[]) {
-    setlocale(LC_ALL, "rus");
-    if (argc < 3)
-    {
-        std::cout << "Wrong input\n";
-        std::cout << "Usage: Files inFile_name, outFile_name\n";
-        return 1;
-    }
-    
-    std::ifstream in(argv[1]);
-    if (!in.is_open())
-    {
-        std::cout << "Input file open error \n";
-        return 2;
-    }
-    else {
-        std::cout << "Input file successfully opened\n";
-    }
-    std::ofstream out(argv[2]);
-    if (!out.is_open())
-    {
-        std::cout << "Output file open error \n";
-        return 3;
-    }
-    else {
-        std::cout << "Output file successfully opened\n";
-    }
-    unsigned char c;
-    std::string str_output = "";
-    int counter = 0, global_counter = 0;
-    while ((c = in.get()) != (unsigned char)EOF)
-    {
-        if (counter == 0)
-            std::cout << std::setfill('0') << std::setw(10) << std::hex << 16 * global_counter << ": ";
-
-        
-        if (c >= 32)
-        {
-            str_output += c;
-            std::cout << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)c << " ";
-        }
-        else if (c != 10)
-        {
-            str_output += c;
-            std::cout << " . ";
-        }
-        counter++;
-
-        if (counter == 8)
-            std::cout << " |  ";
-        else if (counter == 15) {
-            std::cout << "  " + str_output << std::endl;
-            counter = 0;
-            str_output = "";
-            global_counter++;
-        }
-    }
-    if (counter != 0) {
-        while (counter < 15)
-        {
-            std::cout << "  ";
-            if (counter == 7)
-                std::cout << " |  ";
-
-            counter++;
-        }
-        std::cout << "  " + str_output << std::endl;
-    }
-    in.close();
-    out.close();
-    std::cout << "Files are closed successfully (^_^)\n";
-    return 0;
-}
-
-int main(int argc, const char* argv[]) {
-    return file2hex(argc, argv);
+#include <iostream> 
+#include <fstream> 
+#include <iomanip> 
+ 
+int file2hex() { 
+ setlocale(LC_ALL, "rus"); 
+ const char * path_in = "C:/My_Projects_C++/spbu_projects_cpp/in.txt"; 
+ const char * path_out = "C:/My_Projects_C++/spbu_projects_cpp/out.txt"; 
+ std::ifstream in(path_in,std::ios::binary); 
+ if (!in.is_open()) 
+ { 
+ std::cout « "Input file open error \n"; 
+ return -1; 
+ } 
+ else { 
+ std::cout « "Input file successfully opened\n"; 
+ } 
+ std::ofstream out(path_out,std::ios::binary); 
+ if (!out.is_open()) 
+ { 
+ std::cout « "Output file open error \n"; 
+ return 1; 
+ } 
+ else { 
+ std::cout « "Output file successfully opened\n"; 
+ } 
+ unsigned char c; 
+ std::string str_output = ""; 
+ int counter = 0, global_counter = 0; 
+ while ((c = in.get()) != (unsigned char)EOF) 
+ { 
+ if (counter == 0) 
+ out « std::setfill('0') « std::setw(10) « std::hex « 16 * global_counter « ": "; 
+ 
+ 
+ if (c >= 32) 
+ { 
+ str_output += c; 
+ out « std::setfill('0') « std::setw(2) « std::hex « (unsigned int)c « " "; 
+ } 
+ else if (c <= 13) 
+ { 
+ out « " . "; 
+ } 
+ counter++; 
+ 
+ if (counter == 8) 
+ out « " | "; 
+ else if (counter == 16) { 
+ out « " " + str_output « std::endl; 
+ counter = 0; 
+ str_output = ""; 
+ global_counter++; 
+ } 
+ } 
+ if (in.peek() == EOF) { 
+ while (counter < 16) 
+ { 
+ out « " "; 
+ if (counter == 7) 
+ out « " | "; 
+ 
+ counter++; 
+ } 
+ out « " " + str_output « std::endl; 
+ } 
+ in.close(); 
+ out.close(); 
+ std::cout « "Files are closed successfully Mur <3\n"; 
+ return 0; 
+} 
+ 
+int main() { 
+ file2hex(); 
+ return 0; 
 }
