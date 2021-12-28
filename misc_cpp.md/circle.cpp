3ae92410e183c8c591861efbf5ef5e00a46a655c
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-28 05:01:49 +0500

    Update circle.cpp

--- misc_cpp.md/circle.cpp
+++ misc_cpp.md/circle.cpp
@@ -1,20 +1,10 @@
-int x, y;
-std::cout << "enter x, y: ";
-std::cin >> x >> y;
-int r, R;
-std::cout << "enter r, R: ";
-std::cin >> r >> R;
-if (sqrt(pow(x, 2) + pow(y, 2)) <= pow(R, 2) and sqrt(pow(x, 2) + pow(y, 2)) >= pow(r, 2))
-std::cout << "Mur <3\n";
-else
-std::cout << "bob\n";
 double x, y;
-std::cout <<"Enter x, y: ";
-std::cin >> x >> y;
+std::cout « "enter x, y: ";
+std::cin » x » y;
 double r, R;
-std::cout << "enter r, R: ";
-std::cin >> r >> R;
-if (sqrt(pow(x, 2) + pow(y, 2)) <= pow(R, 2) and sqrt(pow(x, 2) + pow(y, 2))>= pow(r, 2))
-std::cout << "Mur <3\n";
+std::cout « "enter r, R: ";
+std::cin » r » R;
+if (sqrt(pow(x, 2) + pow(y, 2)) <= pow(R, 2) and sqrt(pow(x, 2) + pow(y, 2)) >= pow(r, 2))
+std::cout « "Mur <3\n";
 else
-std::cout << "bob\n";
+std::cout « "bob\n";
