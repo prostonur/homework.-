c4fb671e3779b12bff1cc64aae38e0cbe7fae90a
nomatterwhoisit <92108387+prostonur@users.noreply.github.com>
2021-12-28 05:08:33 +0500

    Update task2.cpp

--- files.md/task2.cpp
+++ files.md/task2.cpp
@@ -9,20 +9,20 @@ int file2hex() {
  std::ifstream in(path_in,std::ios::binary); 
  if (!in.is_open()) 
  { 
- std::cout « "Input file open error \n"; 
+ std::cout << "Input file open error \n"; 
  return -1; 
  } 
  else { 
- std::cout « "Input file successfully opened\n"; 
+ std::cout << "Input file successfully opened\n"; 
  } 
  std::ofstream out(path_out,std::ios::binary); 
  if (!out.is_open()) 
  { 
- std::cout « "Output file open error \n"; 
+ std::cout << "Output file open error \n"; 
  return 1; 
  } 
  else { 
- std::cout « "Output file successfully opened\n"; 
+ std::cout << "Output file successfully opened\n"; 
  } 
  unsigned char c; 
  std::string str_output = ""; 
@@ -30,24 +30,24 @@ int file2hex() {
  while ((c = in.get()) != (unsigned char)EOF) 
  { 
  if (counter == 0) 
- out « std::setfill('0') « std::setw(10) « std::hex « 16 * global_counter « ": "; 
+ out << std::setfill('0') << std::setw(10) << std::hex << 16 * global_counter << ": "; 
  
  
  if (c >= 32) 
  { 
  str_output += c; 
- out « std::setfill('0') « std::setw(2) « std::hex « (unsigned int)c « " "; 
+ out << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)c << " "; 
  } 
  else if (c <= 13) 
  { 
- out « " . "; 
+ out << " . "; 
  } 
  counter++; 
  
  if (counter == 8) 
- out « " | "; 
+ out << " | "; 
  else if (counter == 16) { 
- out « " " + str_output « std::endl; 
+ out « " " + str_output << std::endl; 
  counter = 0; 
  str_output = ""; 
  global_counter++; 
@@ -56,17 +56,17 @@ int file2hex() {
  if (in.peek() == EOF) { 
  while (counter < 16) 
  { 
- out « " "; 
+ out << " "; 
  if (counter == 7) 
- out « " | "; 
+ out << " | "; 
  
  counter++; 
  } 
- out « " " + str_output « std::endl; 
+ out << " " + str_output « std::endl; 
  } 
  in.close(); 
  out.close(); 
- std::cout « "Files are closed successfully Mur <3\n"; 
+ std::cout << "Files are closed successfully Mur <3\n"; 
  return 0; 
 } 
  
